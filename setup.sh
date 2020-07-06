0065693868704322eb1b1bc5841fbfedfb2cf7a0
Alex Charrett <github@alex.charrett.com>
2020-07-06 16:54:03 +0100

    adding hashbang to shell script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 aws_region=`grep region: serverless.yml |awk -F': ' '{ print $2 }'`
 ssm_param_name=`grep SsmParamName: serverless.yml |awk -F': ' '{ print $2 }'`
 pip3 install python-twitter --target=.
